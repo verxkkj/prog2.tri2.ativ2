d7a6b9d2f864f637b8c34dcdec618bfcea5a8dfd
Veronica Paz Ledo Balke <167806689+verxkkj@users.noreply.github.com>
2026-06-02 12:49:24 -0300

    prog2.tri2.ativ2

--- src/core.ts
+++ src/core.ts
@@ -1,42 +1,56 @@
 import { Database } from "bun:sqlite";
 
-const db = new Database("database.sqlite");
+const db = new Database("database.sqlite")
 
 db.run(`
   CREATE TABLE IF NOT EXISTS items (
-    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
     title TEXT NOT NULL
   )
-`);
+`)
 
-const querySelectItems = db.prepare("SELECT * FROM items");
-const queryInsertItem = db.prepare("INSERT INTO items (title) VALUES (?)");
-const queryDeleteItem = db.prepare("DELETE FROM items WHERE id = ?");
-const queryUpdateItem = db.prepare("UPDATE items SET title = ? WHERE id = ?");
+const querySelectItems = db.prepare("SELECT * FROM items")
+const queryInsertItem = db.prepare("INSERT INTO items (title) VALUES (?)")
+const queryDeleteItem = db.prepare("DELETE FROM items WHERE id = (?);")
+const queryUpdateItem = db.prepare("UPDATE items SET title = (?) WHERE id = (?)")
 
-export interface Item {
-    id: number;
-    title: string;
+class Item {
+  constructor(public title: string) { }
 }
 
-export class TodoList {
+class TodoList {
+  private items: Item[] = []
 
-    getItems(): Item[] {
-        return querySelectItems.all() as Item[];
-    }
+  addItem(item: Item) {
+    this.items.push(item)
+    queryInsertItem.run(item.title)
+  }
 
-    addItem(title: string): Item {
-        const result = queryInsertItem.run(title);
-        return { id: result.lastInsertRowid as number, title };
+  removeItem(index: number) {
+    if (index >= 0 && index < this.items.length) {
+  const removedItem = this.items[index];
+        if (removedItem){
+            this.items.splice(index,1);
+        }
+        queryDeleteItem.run(index)
+      }
     }
-
-    deleteItem(id: number): boolean {
-        const result = queryDeleteItem.run(id);
-        return result.changes > 0;
-    }
-
-    updateItem(id: number, title: string): boolean {
-        const result = queryUpdateItem.run(title, id);
-        return result.changes > 0;
-    }
+  updateItem(index: number, newTitle: string) {
+    if (index >= 0 && index < this.items.length) {
+            this.items[index].title = newTitle;
+        }
+        queryUpdateItem.run(newTitle, index)
+  }
+  getItems() {
+    const items = querySelectItems.all()
+    return items
+  }
 }
+
+const lista = new TodoList()
+lista.addItem(new Item("estudar"))
+lista.addItem(new Item("ler"))
+lista.addItem(new Item("aprender"))
+lista.removeItem(1)
+lista.updateItem(2, "estudar e ler")
+console.log(lista.getItems())
